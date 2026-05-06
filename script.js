611b9e540ea7e984b43c7f854c311e554a4035d8
Benjamin Lokmic <benjamin.lokmic@fet.ba>
2026-05-06 18:19:45 +0200

    Pamcenje night mode-a

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
+const prekidac = document.getElementById("dugme");
+const tijelo = document.body
+
+const ucitajMod = () => {
+    const trenutnimod =  localStorage.getItem("nightMode");
+    // Ako je nightMode postavljen na "true", primijeni tamni mod
+    if (trenutnimod === "true") {
+        tijelo.classList.add("dark-mode");
+        prekidac.innerText = "Day Mode";
+    } else {
+        tijelo.classList.remove("dark-mode");
+        prekidac.innerText = "Night Mode";
+    }
+};
+
+ucitajMod(); // Poziv funkcije prilikom učitavanja stranice
 function toggleNightMode() {
-    // Ova funkcija se izvršava čim klikneš na dugme
+    // Ova funkcija se izvršava čim se klikne na dugme
     document.body.classList.toggle("dark-mode");
     
-    // Možeš promijeniti i tekst na dugmetu da bude jasnije
+   
     const dugme = document.getElementById("dugme");
     if (document.body.classList.contains("dark-mode")) {
         dugme.innerText = "Day Mode";
+
+        localStorage.setItem("nightMode", "true"); // Spremi stanje u localStorage
     } else {
         dugme.innerText = "Night Mode";
+        localStorage.setItem("nightMode", "false"); // Spremi stanje u localStorage
     }
 }
