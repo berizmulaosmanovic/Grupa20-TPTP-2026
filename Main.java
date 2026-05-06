c1a1d292e168347e57b2987d0cc3bc70aae8a10f
benjaminlokmic <benjamin.lokmic@fet.ba>
2026-05-06 16:34:12 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,5 @@
+public class Main { // Class name must match the filename (Main.java)
+    public static void main(String[] args) { // Entry point
+        System.out.println("Hello, World!"); // Prints text to console
+    }
+}
