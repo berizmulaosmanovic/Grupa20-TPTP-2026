81865eb2ae8cd2176876b63e09efff0ccd5ba058
Benjamin Lokmic <benjamin.lokmic@fet.ba>
2026-05-06 17:35:51 +0200

    Osnovne postavke stranice i night mode

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-public class Main { // Ime klase mora biti kao ime fajla
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+function toggleNightMode() {
+    // Ova funkcija se izvršava čim klikneš na dugme
+    document.body.classList.toggle("dark-mode");
+    
+    // Možeš promijeniti i tekst na dugmetu da bude jasnije
+    const dugme = document.getElementById("dugme");
+    if (document.body.classList.contains("dark-mode")) {
+        dugme.innerText = "Day Mode";
+    } else {
+        dugme.innerText = "Night Mode";
     }
 }
