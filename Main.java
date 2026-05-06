aa6977ec8b00b813ea0ca22a699f51eb9dd69f51
Benjamin Lokmic <benjamin.lokmic@fet.ba>
2026-05-06 17:23:26 +0200

    Izmjena

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
+
 public class Main { // Class name must match the filename (Main.java)
     public static void main(String[] args) { // Entry point
         System.out.println("Hello, World!"); // Prints text to console
+    }
+}
+=======public class Main { // Ime klase mora biti kao ime fajla
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
     }
 }
---
+++ script.js
@@ -0,0 +1,5 @@
+public class Main { // Ime klase mora biti kao ime fajla
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
