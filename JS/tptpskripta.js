430ec5be74289361a9b8177d79aa94f83a14e8ea
benjamindzindo-svg <benjamin.dzindo@fet.ba>
2026-05-09 02:36:43 +0100

    Ispravljene 'NightMode' i 'Filtriraj' funkcije, te greška u 'Timer' funkciji.

--- JS/tptpskripta.js
+++ JS/tptpskripta.js
@@ -16,18 +16,22 @@ const ucitajMod = () => {
 ucitajMod(); // Poziv funkcije prilikom učitavanja stranice
 function toggleNightMode() {
     // Ova funkcija se izvršava čim se klikne na dugme
-    document.body.classList.toggle("dark-mode");
+    tijelo.classList.toggle("dark-mode");
+
+// Ispravio i rasčistio funkciju jer su 'const' već zadane
     
-   
-    const dugme = document.getElementById("dugme");
-    if (document.body.classList.contains("dark-mode")) {
-        dugme.innerText = "Day Mode";
+    if (tijelo.body.classList.contains("dark-mode")) {
+        prekidac.innerText = "Day Mode";
 
         localStorage.setItem("nightMode", "true"); // Spremi stanje u localStorage
     } else {
-        dugme.innerText = "Night Mode";
+        prekidac.innerText = "Night Mode";
         localStorage.setItem("nightMode", "false"); // Spremi stanje u localStorage
     }
+} // Izvukao funkciju filtriraj od toggleNightMode jer su to dvije odvojene funkcije.
+
+prekidac.addEventListener("click", toggleNightMode); // Dodajemo event listener na dugme da pozove funkciju toggleNightMode kada se klikne
+
     function filtriraj(kategorija) { // Postaviti id "kartica" na odgovarajuce elemente i
                                       //  pozvati funkciju sa odgovarajućom kategorijom   
            const sveKartice = document.querySelectorAll(".kartica");
@@ -43,7 +47,7 @@ function toggleNightMode() {
         // Prvo nam treba početni datum, koji je 29.10.1972. 
         // Zatim ćemo izračunati razliku između tog datuma i trenutnog vremena
         // Na kraju ćemo tu razliku pretvoriti u dane, sate, minute i sekunde
-        const pocetnidatum = new Date("October 29, 1972 ").getTime;
+        const pocetnidatum = new Date("October 29, 1972 ").getTime(); 
         const sada = new Date().getTime();
         const razlika = sada - pocetnidatum;
         
@@ -55,4 +59,4 @@ function toggleNightMode() {
     }
     setInterval(azurirajTimer, 1000); // Ova funkcija će se pozivati svakih 1000 milisekundi (1 sekund)
 azurirajTimer(); // Poziv funkcije odmah da ne čekamo prvi interval
-}
+
